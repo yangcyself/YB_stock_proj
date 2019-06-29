76ad74ff8149c004553c98d906676f86578be00d
Yangcy <13520183356@139.com>
2019-06-29 22:52:04 +0800

    added test mode and many output items in env

--- gym-stock/gym_stock/envs/stock_env.py
+++ gym-stock/gym_stock/envs/stock_env.py
@@ -11,13 +11,16 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 Feature_num = 137
+maxstep = 200
 filePath = os.path.join(os.path.dirname(os.path.realpath(__file__)),"envdata")
+outfilePath = os.path.join(os.path.dirname(os.path.realpath(__file__)),"envoutdata")
 allNumbers = set([i for i in range(100)])
 testNumbers = set([i for i in range(0,100,7)])
 trainNumbers = allNumbers - testNumbers
-inputDataNames = [os.path.join(filePath,"%denv.pkl"%i) for i in trainNumbers]
+MODE = "TEST"
+# MODE = "TRAIN"
+inputDataNames = [os.path.join(filePath,"%denv.pkl"%i) for i in (trainNumbers if MODE != "TEST" else testNumbers)] 
 ChangFile_num = 50
-
 class StockEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     # all the data files are in the form of pickle, saved as pandas dataframe
@@ -41,7 +44,8 @@ class StockEnv(gym.Env):
         self.deltaTimeThresh = pd.Timedelta('600s')
         
         self.change_file_count = 0
-    
+        self.history = None
+        self.fileind = 0
     def _concateObs(self,stock,hand):
         return np.array(list(stock)+[int(hand)])
 
@@ -53,10 +57,15 @@ class StockEnv(gym.Env):
         self.index += 1
         tmp_obs = self.current.iloc[self.index:self.index+1]
         tmp_obs = tmp_obs.drop(columns = ["UpdateMillisec","UpdateTime"])
+        self.actionCounts[action] += 1
         # print(tmp_obs.columns[119],tmp_obs.columns[108],tmp_obs.columns[118])
+        # if(len(tmp_obs.values)==0):
+        # print(self.index,len(self.current.index))
         stock_obs = tmp_obs.values[0]
-        done = (self.index >= len(self.current.index) or self.current.iloc[self.index]["UpdateTime"] - 
-                self.current.iloc[self.index-1 ]["UpdateTime"] >  self.deltaTimeThresh )
+        self.maxstep -= 1
+        done = (self.index+1 >= len(self.current.index) or self.current.iloc[self.index]["UpdateTime"] - 
+                self.current.iloc[self.index-1 ]["UpdateTime"] >  self.deltaTimeThresh 
+                or (self.maxstep<=0 and MODE!="TEST"))
         rwd = (old_hand-self.hands) * stock_obs[self.action_prize[action]]
         hand_obs = np.array(self.hands)
 
@@ -66,18 +75,34 @@ class StockEnv(gym.Env):
         #rwd can be too large
         rwd /=5000
 
-        return self._concateObs(stock_obs,hand_obs-5), rwd, done, None
+        info = {}
+        totalActions = sum([self.actionCounts[i] for i in range(3)])
+        info["EpisodeHoldPercentage"] = self.actionCounts[1]
+        info["EpisodeSellPercentage"] = self.actionCounts[2]
+        info["EpisodeBuyPercentage"] = self.actionCounts[0]
+        self.history["midprice"].append(self.stock_obs[self.action_prize[1]]) # the midprice
+        self.history["action"].append(action)
+        self.history["hands"].append(hand_obs)
+        return self._concateObs(stock_obs,hand_obs-5), rwd, done, info
 
     def reset(self):
-        if(self.current is None or self.change_file_count > ChangFile_num): 
-            selectedFile = random.choice(self.inputDataNames)
-            pd.read_pickle(selectedFile)
-            self.current = pd.read_pickle(selectedFile)
-            self.change_file_count = 0
-            print("loaded: ", selectedFile)
-
+        if(MODE=="TEST"):
+            with open(os.path.join(outfilePath,"%dhistory.pkl" %self.fileind),"wb") as f:
+                f.dump(self.history)
+            self.history = {k:[] for k in ["midprice","action","hands"] }
+            selectedFile = self.inputDataNames[self.fileind]
+            self.fileind = (self.fileind + 1) % len(self.inputDataNames)
+        else:
+            if(self.current is None or (self.change_file_count > ChangFile_num)): 
+                selectedFile = random.choice(self.inputDataNames)
+                # pd.read_pickle(selectedFile)
+                self.change_file_count = 0
+        self.current = pd.read_pickle(selectedFile)
+        print("loaded: ", selectedFile)
+        self.maxstep = maxstep
         self.change_file_count += 1
         self.index = random.randint(0,len(self.current.index)-1)
+        self.actionCounts = [0]*3
         # find the begining of the day
         forward_count = 0
         while(self.index>0 and forward_count < 200 and 
