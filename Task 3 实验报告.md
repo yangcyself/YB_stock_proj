69413fdb5e116e3b1d55bdfeb64cd8393509c66f
Yangcy <13520183356@139.com>
2019-06-30 17:14:06 +0800

    added report, the data augmentor in env

---
+++ Task 3 实验报告.md
@@ -0,0 +1,128 @@
+# Task 3 experiment Report
+
+The task is to train an agent to invest in the market and earn as much profit as possible. The raw observation in this task is the features of the market, and the action space is three discrete choices, namely:  to hold, to sell one hand, and to buy one hand. As the task is simply choose actions according to observation, a naïve method is the train a predictor to predict the rising and falling trends in the near future, and then simply let the agent to buy when the predictor predicts price rising. We use this method as a baseline to test the ability to earn money of our agent. In the following sections, we will compare different reward function. And we will test the effect of adding in different data augmentation methods. Then we will test different network architectures. After this, we will compare two sets of reinforcement learning algorithms--Value function based methods like DQN or Policy Gradient based algorithms like DDPG, TRPO or PPO. At last, we adopt a new kind of network architecture, namely value iteration network in this task. The biggest characteristic of this algorithm is it is model based. As we can know exactly how many hands we will hold after a certain action, we hope this model based algorithm is suitable for the task.
+
+
+
+## The Stock Simulation Environment
+
+The stock simulation environment takes in the raw data as simulate the rises and falls of the stock market according to the raw data. Each step is a line in the data. In each episode, it is guaranteed that the difference of time stamps of each neighboring steps is less than 1 minute. 
+
+### Dataset
+
+Although generally reinforcement learning have no dataset, but for this typical task, we have to use the real dataset to represent a real stock market. Together with the notion of dataset, we also have to divide the train and test dataset. In our experiments, we divided the whole dataset into 100 sections, and use 70% of them as train datasets and 30% of them as test datasets.
+
+### rewards
+
+In reinforcement learning, as the agent tries its best to maximize the reward, the reward has huge impact on its behavior. We have designed some rewards and tested their effect.
+
+Note that in the following experiments, we have divided the earned reward by 5000 to keep the loss and gradient of neural network training in a reasonable range
+
+#### direct rewards
+
+The most straight forward reward that comes to our mind is to simulate the money payed and earned in the action of buy and sell stocks. The reward can be expressed as following:
+$$
+R_{direct}(t) = (Hand(t-1)-Hand(t))*(price(t,a(t)))
+$$
+The $a(t)$ is the action at time $t$, and the $price(t,a)$ is the price related to the action--sell price for sell action and buy price for buy action. Note that the reward is negative if the agent buys, this might make the agent not willing to buy stock in the following experiments. 
+
+But this reward is still suitable as the aim of agent is to maximize the total discounted reward.
+$$
+R = \sum_t\gamma^tR_{direct}(t)
+$$
+We choose $\gamma = 1$ and then the reward of each action is the total income by a series of buy and sell actions.
+
+#### accumulated rewards
+
+From the discussion with other groups, we found that some groups got good results with accumulated reward functions.  
+$$
+R_{accumulate}(t) = \sum_{\tau = 0}^{t} R_{direct}(\tau)
+$$
+We cannot understand why this kind of reward can yield a good performance, as the rewards do not represent the ability of earning money at present or in the future, but the money earned in the past. However we do tested this reward for the sake of completeness and practicality .
+
+#### asset rewards
+
+In the above reward functions, the number of stocks currently hold by the agent does not contribute directly to the reward. We can change this by introducing asset rewards.
+$$
+R_{asset}(t) = R_{direct}(t) + Hand(t)*midprice(t)
+$$
+This means the when the agent takes a "buy" action, it does not actually pay all the money, its money is just changed into a another form of assets.
+
+### observations
+
+Another key issue about the training a good agent is the observation.  As shown in the first and second tasks, simply feed the raw features to the network might is not a feasible solution. Hence, we tried data augmentation based on the techniques explored in the first two tasks. However, we have to keep in mind that the data augmentation should not use the future data, otherwise, we are using future to predict the future. 
+
+
+
+## Base Line
+
+
+
+## Basic Case
+
+The most basic experiment is DQN with a two hidden layer MLP network structure, with no data augmentation, and direct reward(pay money for buying and earn money for selling). The learning curve is as following.
+
+![1561823347301](D:\yangcy\UNVjunior\EE359\Projects\stockRL\DQN-reward-0.png)
+
+On one typical test dataset, the price and hand number scatter graph is following, where the x and y axes of the points means the time and price of the market, and the color means the number of hands hold at that moment. The lighter the color, the more hands the agent holds.
+
+![1561827962147](D:\yangcy\UNVjunior\EE359\Projects\stockRL\pics\DQN-0-hand-scatter0.png)
+
+We can see that generally the agent holds more hands when the price is low(at about 4000 step)  in the hope of a bounce back. And we can zoom in to find that the agent can correctly buy in stocks before a rise and hold till the market goes down. 
+
+![1561827909927](D:\yangcy\UNVjunior\EE359\Projects\stockRL\pics\DQN-0-hand-scatter.png)
+
+At the step about 3500 the agent is more likely to buy in as the price is low. The zoomed view is as following. We can see that the agent holds stock at about 3500 steps with the price goes down. This mistake can cost the agent money, but later on it correctly bought stocks before a summit.
+
+![1561828428147](D:\yangcy\UNVjunior\EE359\Projects\stockRL\pics\DQN-0-hand-scatter1.png)
+
+We observed that the network prone to converge to a local maxima as the trained steps increases. Here the local maxima is to sell the initial hands but seldom buy any stock. This phenomenon might because  the direct reward gives positive reward every time the agent choose a sell action, and give a negative reward every time the agent choose a buy action. Although the total reward is the actual money earned by the agent, the reward directly penalize the buy action and makes the agent less likely to buy. Or maybe this is because the agent's poor ability to make predictions, which can be alleviated by data augmentation, or a more expressive network structure.
+
+### Basic case with accumulated rewards
+
+Using the accumulated rewards described above, we get the following learning curve.
+
+![1561879501856](D:\yangcy\UNVjunior\EE359\Projects\stockRL\pics\DQN-accuR1.png)
+
+This looks similar to the curve with direct rewards, but the curve of the sell action percentage is interesting.
+
+![1561879630542](D:\yangcy\UNVjunior\EE359\Projects\stockRL\pics\DQN-accuRSellPerc.png)
+
+This is the plot about the percentage of the agent to choose "Sell" action. Note that the agent can still chose sell action even when its short hands reaches its limit, the effect in this case is just same as "hold". This means to things, the first is that the agent still converge to the local maximum of not buy at all.  Another is that the agent some how learned to buy some stocks or to delay the "sell" action, but it found at last that this kind of action cannot increase the reward. This is very simple to explain, this is because if the agent can sell the stocks as soon as possible, it the earned reward is added in all the following steps. 
+
+### Basic case with asset rewards
+
+
+
+## LSTM Model
+
+Although some people say that using Recurrent Neural Networks in reinforcement learning tasks is a violation of the Markov assumption, using LSTM in reinforcement learning neural network architecture is a common practice. Thus we tried to use LSTM as the network structure.
+
+The behavior of LSTM  agent on test dataset can be shown as the following:
+
+![1561884768057](D:\yangcy\UNVjunior\EE359\Projects\stockRL\pics\LSTMDQNhand0.png)
+
+From which we can see that it learnt to not hold stocks when the market has a falling trend. And to hold stocks when the stock is going up.
+
+However in this episode, the agent failed to money
+
+>PROFIT: -979.0
+>AveBuyPrice: 5611.262403528115
+>AveSellPrice: 5610.193832599119
+
+We looked at some other episodes, and found the model fails to earn money in most cases
+
+## Data Augmentation 
+
+
+
+
+
+## DDPG
+
+
+
+
+
+## Value Iteration Networks
+
--- gym-stock/gym_stock/envs/stock_env.py
+++ gym-stock/gym_stock/envs/stock_env.py
@@ -9,6 +9,8 @@ import os
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import pickle as pkl
+from sklearn.decomposition import PCA 
 
 Feature_num = 137
 maxstep = 200
@@ -17,10 +19,66 @@ outfilePath = os.path.join(os.path.dirname(os.path.realpath(__file__)),"envoutda
 allNumbers = set([i for i in range(100)])
 testNumbers = set([i for i in range(0,100,7)])
 trainNumbers = allNumbers - testNumbers
-MODE = "TEST"
-# MODE = "TRAIN"
+# MODE = "TEST"
+MODE = "TRAIN"
+# ACCUMUREWARD = True
+# ACCUMUREWARD = False
+REWARDKIND = ["DIRECT","ACCUMULATE","ASSET"][2]
+initHand = 0
+obsLenth = 20
 inputDataNames = [os.path.join(filePath,"%denv.pkl"%i) for i in (trainNumbers if MODE != "TEST" else testNumbers)] 
 ChangFile_num = 50
+OBS_HANDS = False
+trainAugLength = 200 # the number of data steps used to train the Augmentor
+
+class Augmentor():
+    """
+    The dataAugmentor class
+    used to augment the observation data
+    """
+    def fit(self,X):
+        """
+        fit a bunch of data to learn the parameter of aumentor
+        """
+        pass
+    def transform(self,X):
+        """
+        input the observation X and return the augmented observation
+        """
+        pass
+
+class NormAugmentor(Augmentor):
+    """
+        Normalize the observation so that the average is 0 and the variation is 1
+    """
+    def __init__(self):
+        self.outputdim = Feature_num
+    def fit(self,X):
+        """
+        X is a two dim N*P array
+        """
+        self.ave = np.mean(X, axis = 0)
+        self.std = np.var(X,axis = 0) +0.5
+    def transform(self,X):
+        X = (X-self.ave)/self.std
+        return X
+
+class PCAAugmentor(Augmentor):
+    """
+        Use PCA to do the dimention reduction, after Normalized the data
+    """
+    def __init__(self,outputdim = 42):
+        self.outputdim = outputdim
+        self.normalizer = NormAugmentor()
+        self.pca = PCA(n_components = outputdim)
+    def fit(self,X):
+        self.normalizer.fit(X)
+        normed = self.normalizer.transform(X)
+        self.pca.fit(normed)
+    def transform(self,X):
+        normed = self.normalizer(X)
+        return self.pca.transform(normed)
+
 class StockEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     # all the data files are in the form of pickle, saved as pandas dataframe
@@ -28,8 +86,8 @@ class StockEnv(gym.Env):
 
     def __init__(self,inputdataNames = inputDataNames):
         # the length of the observation is 137 (not including hand)
-        high = np.ones((137,))
-        high = np.array(list(high)+[5])
+        high = np.ones((obsLenth,137))
+        # high = np.array(list(high)+[5])
         self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(low = -high,high = high,dtype = np.float32)
 
@@ -39,13 +97,15 @@ class StockEnv(gym.Env):
         self.index = 0
         
         self.action_hand = [-1,0,1] # mapping the change of hand and the action 
-        self.action_prize = [119,108,118] # the index of bidprice1, midprice, Askprice
+        self.action_prize = [119-137,108-137,118-137] # the index of bidprice1, midprice, Askprice
         
         self.deltaTimeThresh = pd.Timedelta('600s')
         
         self.change_file_count = 0
         self.history = None
         self.fileind = 0
+        self.augmentor = NormAugmentor()
+
     def _concateObs(self,stock,hand):
         return np.array(list(stock)+[int(hand)])
 
@@ -55,53 +115,73 @@ class StockEnv(gym.Env):
         self.hands = min(10,self.hands)
         self.hands = max(0,self.hands)
         self.index += 1
-        tmp_obs = self.current.iloc[self.index:self.index+1]
+        tmp_obs = self.current.iloc[self.index:self.index+obsLenth]
         tmp_obs = tmp_obs.drop(columns = ["UpdateMillisec","UpdateTime"])
         self.actionCounts[action] += 1
         # print(tmp_obs.columns[119],tmp_obs.columns[108],tmp_obs.columns[118])
         # if(len(tmp_obs.values)==0):
         # print(self.index,len(self.current.index))
-        stock_obs = tmp_obs.values[0]
+        stock_obs = tmp_obs.values#.reshape(-1)
         self.maxstep -= 1
-        done = (self.index+1 >= len(self.current.index) or self.current.iloc[self.index]["UpdateTime"] - 
+        done = (self.index+obsLenth >= len(self.current.index) or self.current.iloc[self.index]["UpdateTime"] - 
                 self.current.iloc[self.index-1 ]["UpdateTime"] >  self.deltaTimeThresh 
                 or (self.maxstep<=0 and MODE!="TEST"))
-        rwd = (old_hand-self.hands) * stock_obs[self.action_prize[action]]
+        rwd = (old_hand-self.hands) * stock_obs[-1][self.action_prize[action]]
         hand_obs = np.array(self.hands)
 
         if(done):
-            rwd +=  self.hands* stock_obs[self.action_prize[0]]
+            rwd +=  self.hands* stock_obs[-1][self.action_prize[0]]
 
         #rwd can be too large
         rwd /=5000
 
+        if(REWARDKIND=="ACCUMULATE"):
+            self.accumulated_reward += rwd
+            rwd = self.accumulated_reward
+        elif(REWARDKIND=="ASSET"):
+            rwd += self.hands*stock_obs[-1][self.action_prize[0]] # add the bid price as the asset saved in the market
+
         info = {}
         totalActions = sum([self.actionCounts[i] for i in range(3)])
-        info["EpisodeHoldPercentage"] = self.actionCounts[1]
-        info["EpisodeSellPercentage"] = self.actionCounts[2]
-        info["EpisodeBuyPercentage"] = self.actionCounts[0]
-        self.history["midprice"].append(self.stock_obs[self.action_prize[1]]) # the midprice
-        self.history["action"].append(action)
-        self.history["hands"].append(hand_obs)
-        return self._concateObs(stock_obs,hand_obs-5), rwd, done, info
+        info["EpisodeHoldPercentage"] = self.actionCounts[1] /sum(self.actionCounts)
+        info["EpisodeSellPercentage"] = self.actionCounts[0]/sum(self.actionCounts)
+        info["EpisodeBuyPercentage"] = self.actionCounts[2]/sum(self.actionCounts)
+        if(MODE=="TEST"):
+            self.history["bidprice"].append(stock_obs[-1][self.action_prize[0]])
+            self.history["midprice"].append(stock_obs[-1][self.action_prize[1]]) # the midprice
+            self.history["askprice"].append(stock_obs[-1][self.action_prize[2]]) 
+            self.history["action"].append(action)
+            self.history["hands"].append(hand_obs)
+        # print("len(self._concateObs(stock_obs,hand_obs-5))",len(self._concateObs(stock_obs,hand_obs-5)))
+        if(self.augmentor is not None):
+            stock_obs = self.augmentor.transform(stock_obs)
+
+        if(OBS_HANDS):
+            return self._concateObs(stock_obs,hand_obs-5), rwd, done, info
+        else:
+            return stock_obs, rwd, done, info
+
 
     def reset(self):
         if(MODE=="TEST"):
             with open(os.path.join(outfilePath,"%dhistory.pkl" %self.fileind),"wb") as f:
-                f.dump(self.history)
-            self.history = {k:[] for k in ["midprice","action","hands"] }
+                pkl.dump(self.history,f)
+            self.history = {k:[] for k in ["midprice","action","hands","bidprice","askprice"] }
             selectedFile = self.inputDataNames[self.fileind]
             self.fileind = (self.fileind + 1) % len(self.inputDataNames)
+            self.current = pd.read_pickle(selectedFile)
+            self.index = 0
         else:
             if(self.current is None or (self.change_file_count > ChangFile_num)): 
                 selectedFile = random.choice(self.inputDataNames)
                 # pd.read_pickle(selectedFile)
                 self.change_file_count = 0
-        self.current = pd.read_pickle(selectedFile)
-        print("loaded: ", selectedFile)
+                self.current = pd.read_pickle(selectedFile)
+                print("loaded: ", selectedFile)
+
+            self.index = random.randint(0,len(self.current.index)-1)
         self.maxstep = maxstep
         self.change_file_count += 1
-        self.index = random.randint(0,len(self.current.index)-1)
         self.actionCounts = [0]*3
         # find the begining of the day
         forward_count = 0
@@ -110,17 +190,27 @@ class StockEnv(gym.Env):
                 self.current.iloc[self.index - 1]["UpdateTime"] <  self.deltaTimeThresh ):
             self.index = self.index - 1
             forward_count +=1
-        
+        if(self.augmentor is not None):
+            trainAug = self.current.iloc[self.index:self.index + trainAugLength].drop(columns = ["UpdateMillisec","UpdateTime"]).values
+            self.augmentor.fit(trainAug)
+            self.index += trainAugLength
         print("start from: ",self.index)
         # self.hands = 5
-        self.hands = random.randint(0,10)
+        self.hands = random.randint(0,10) if initHand<0 else initHand
         print("epoInitHand:",self.hands)
-        tmp_obs = self.current.iloc[self.index:self.index+1]
+        tmp_obs = self.current.iloc[self.index:self.index+obsLenth]
         tmp_obs = tmp_obs.drop(columns = ["UpdateMillisec","UpdateTime"])
         # print(tmp_obs.columns.values)
-        stock_obs = tmp_obs.values[0]
+        stock_obs = tmp_obs.values#.reshape(-1)
         # print(stock_obs.shape)
-        return self._concateObs(stock_obs, np.array(self.hands-5))
+        if(REWARDKIND=="ACCUMULATE"):
+            self.accumulated_reward = 0
+        if(self.augmentor is not None):
+            stock_obs = self.augmentor.transform(stock_obs)
+        if(OBS_HANDS):
+            return self._concateObs(stock_obs, np.array(self.hands-5))
+        else:
+            return stock_obs
     def render(self, mode='human'):
         pass           
 
