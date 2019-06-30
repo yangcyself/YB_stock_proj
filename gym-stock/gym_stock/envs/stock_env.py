099a5a811b4d62e5fb73c0c2c2b2f3414f969b7f
Yangcy <13520183356@139.com>
2019-06-30 19:18:50 +0800

    added obsnum in stockenv

--- gym-stock/gym_stock/envs/stock_env.py
+++ gym-stock/gym_stock/envs/stock_env.py
@@ -87,7 +87,8 @@ class StockEnv(gym.Env):
 
     def __init__(self,inputdataNames = inputDataNames):
         # the length of the observation is 137 (not including hand)
-        high = np.ones((obsLenth,137))
+        obsnum = 42
+        high = np.ones((obsLenth,obsnum))
         # high = np.array(list(high)+[5])
         self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(low = -high,high = high,dtype = np.float32)
@@ -106,7 +107,7 @@ class StockEnv(gym.Env):
         self.history = None
         self.fileind = 0
         # self.augmentor = NormAugmentor()
-        self.augmentor = PCAAugmentor()
+        self.augmentor = PCAAugmentor(obsnum)
 
     def _concateObs(self,stock,hand):
         return np.array(list(stock)+[int(hand)])
